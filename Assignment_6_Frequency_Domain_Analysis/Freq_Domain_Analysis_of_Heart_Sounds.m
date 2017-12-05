61fb9f4773b6276de205e8129549e96520209c81
eniemela3 <eniemela3@gmail.com>
2017-12-05 11:20:24 +0200

    Completed Assignment 6

--- Assignment_6_Frequency_Domain_Analysis/Freq_Domain_Analysis_of_Heart_Sounds.m
+++ Assignment_6_Frequency_Domain_Analysis/Freq_Domain_Analysis_of_Heart_Sounds.m
@@ -31,12 +31,13 @@ end
 %     title(string(fields{i}));
 % end
 
-%% 4, 5
+%% 4, 5, 6
 peaks = cell(5,1);
 window_len = 300; % ms
 all_systoles = cell(5,1);
-welches = cell(5,1);
+all_psds = cell(5,1);
 avg_psds = cell(5,1);
+mean_freqs = [];
 
 for i = 1:length(peaks)
     field_ecg = join(['ECG_pat' string(i)], "");
@@ -53,16 +54,44 @@ for i = 1:length(peaks)
     subplot(3,1,2);
     plot(data.(field_pcg)(peakOnsets(1):peakOnsets(3)));
     subplot(3,1,3);
-    spectrogram(data.(field_pcg)(peakOnsets(1):peakOnsets(3)), 50, 45, [], Fs, 'MinThreshold', -80 );
+    spectrogram(data.(field_pcg)(peakOnsets(1):peakOnsets(3)), 50, 45, [], Fs, 'MinThreshold', -30, 'yaxis');
     
-    % 6
+    % 6, 7
     systoles = [];
+    welches = [];
+    meanwelches = [];
+    
     for j = 1:length(peakOnsets)
         try
             systole = data.(field_pcg)(peakOnsets(j):peakOnsets(j) + window_len);
             systoles(j,:) = systole;
+            [welch, freq] = pwelch(systole, [], [], [], Fs);
+            %meanwelch = mean(welch);
+            %meanwelches(j) = meanwelch;
+            welches(j,:) = welch;
         catch ME
         end     
     end
     all_systoles{i} = systoles;
+    all_psds{i} = welches;
+    avg_psds{i} = mean(welches);
+    mean_freqs(i) = meanfreq(avg_psds{i}, freq);
 end
+
+%% 7
+figure;
+for i = 1:5
+    subplot(5,3,3*i-2);
+    plot(all_systoles{i}(1,:));
+    title(join(['Patient ' string(i) ', First systole'], ""));
+    xlabel('Time (s)');
+    subplot(5,3,3*i-1);
+    plot(freq, all_psds{i}(1,:));
+    title('PSD');
+    xlabel('Frequency (Hz)');
+    subplot(5,3,3*i);
+    plot(freq, avg_psds{i});
+    title('Mean PSD');  
+    xlabel('Frequency (Hz)');
+end
+    
